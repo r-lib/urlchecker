2ccd9c31709cd8b7abf8917a34e731776da2b8e2
Jim Hester <james.f.hester@gmail.com>
2020-12-01 09:13:24 -0500

    Note where the function came from

--- R/rmd.R
+++ R/rmd.R
@@ -19,7 +19,8 @@ function(dir)
     url_db(urls, path)
 }
 
-
+# adapted from https://github.com/wch/r-source/blob/58d223cf3eaa50ff8cfc2caf591d67350e549e4a/src/library/tools/R/utils.R#L1847-L1857
+# Adding the autolink_bare_uris extension
 .pandoc_md_for_CRAN2 <- function(ifile, ofile) {
     asNamespace("tools")$.system_with_capture("pandoc", paste(shQuote(normalizePath(ifile)),
         "-s", "--mathjax", "--email-obfuscation=references", "-f", "markdown+autolink_bare_uris",

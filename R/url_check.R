e941775360831b84ddf99b31824ed5c2d8b373ac
Jim Hester <james.f.hester@gmail.com>
2021-03-02 09:53:53 -0500

    Move functions into separate files

--- R/url_check.R
+++ R/url_check.R
@@ -23,44 +23,6 @@ url_check <- function(path = ".", db = NULL, parallel = TRUE, pool = curl::new_p
   res
 }
 
-#' Update URLs in a package
-#'
-#' @param path Path to the package
-#' @param results results from [url_check].
-#' @return The results from `url_check(path)`, invisibly.
-#' @export
-#' @examples
-#' \dontrun{
-#' url_update("my_pkg")
-#' }
-#'
-url_update <- function(path = ".", results = url_check(path)) {
-  can_update <- vlapply(results[["New"]], nzchar)
-  to_update <- results[can_update, ]
-  for (row in seq_len(NROW(to_update))) {
-    old <- to_update[["URL"]][[row]]
-    new <- to_update[["New"]][[row]]
-    root <- to_update[["root"]][[row]]
-    if (nzchar(new)) {
-      from <- to_update[["From"]][[row]]
-      if (("README.md" %in% from) && file.exists("README.Rmd")) {
-        from <- c(from, "README.Rmd")
-      }
-      for (file in from) {
-        file_path <- file.path(root, file)
-        data <- readLines(file_path)
-        data <- gsub(old, new, data, fixed = TRUE)
-        writeLines(data, file_path)
-        cli::cli_alert_success("{.strong Updated:} {.url {old}} to {.url {new}} in {.file {file}}")
-      }
-    }
-  }
-
-  print(results[!can_update, ])
-
-  invisible(results)
-}
-
 #' @export
 print.urlchecker_db <- function(x, ...) {
   if (NROW(x) == 0) {
@@ -115,4 +77,3 @@ print.urlchecker_db <- function(x, ...) {
   invisible(x)
 }
 
-vlapply <- function(x, f, ...) vapply(x, f, logical(1))
---
+++ R/url_update.R
@@ -0,0 +1,37 @@
+#' Update URLs in a package
+#'
+#' @param path Path to the package
+#' @param results results from [url_check].
+#' @return The results from `url_check(path)`, invisibly.
+#' @export
+#' @examples
+#' \dontrun{
+#' url_update("my_pkg")
+#' }
+#'
+url_update <- function(path = ".", results = url_check(path)) {
+  can_update <- vlapply(results[["New"]], nzchar)
+  to_update <- results[can_update, ]
+  for (row in seq_len(NROW(to_update))) {
+    old <- to_update[["URL"]][[row]]
+    new <- to_update[["New"]][[row]]
+    root <- to_update[["root"]][[row]]
+    if (nzchar(new)) {
+      from <- to_update[["From"]][[row]]
+      if (("README.md" %in% from) && file.exists("README.Rmd")) {
+        from <- c(from, "README.Rmd")
+      }
+      for (file in from) {
+        file_path <- file.path(root, file)
+        data <- readLines(file_path)
+        data <- gsub(old, new, data, fixed = TRUE)
+        writeLines(data, file_path)
+        cli::cli_alert_success("{.strong Updated:} {.url {old}} to {.url {new}} in {.file {file}}")
+      }
+    }
+  }
+
+  print(results[!can_update, ])
+
+  invisible(results)
+}
